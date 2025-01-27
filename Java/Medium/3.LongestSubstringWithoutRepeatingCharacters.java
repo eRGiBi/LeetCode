5bd632c773540aeaa5b9ab2903f581334797816b
eRGiBi <xx4qwexx@gmail.com>
2025-01-27 01:32:17 +0100

    +

--- Java/Medium/3.LongestSubstringWithoutRepeatingCharacters.java
+++ Java/Medium/3.LongestSubstringWithoutRepeatingCharacters.java
@@ -28,3 +28,57 @@ class Solution {
         return longestSubLength;
     }
 }
+
+class Solution2 {
+    public int lengthOfLongestSubstring(String s) {
+
+        HashSet<Character> currentSequenceCharacters = new HashSet<>();
+        int longestSubLength = 0;
+        int left = 0;
+
+        if (s.length() == 1) return 1;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            if (currentSequenceCharacters.contains(s.charAt(right)) ) {
+
+                do {
+                    currentSequenceCharacters.remove(s.charAt(left));
+                    left++;
+
+                } while (currentSequenceCharacters.contains(s.charAt(right)));
+
+                currentSequenceCharacters.add(s.charAt(right));
+
+            }
+            else {
+                currentSequenceCharacters.add(s.charAt(right));
+                longestSubLength = Math.max(longestSubLength, right - left + 1);
+            }
+        }
+        return longestSubLength;
+    }
+}
+
+class Solution3 {
+    public int lengthOfLongestSubstring(String s) {
+
+        int n = s.length();
+        int longestSubLength = 0;
+
+        int[] charIndex = new int[128];
+        Arrays.fill(charIndex, -1);
+
+        int left = 0;
+
+        for (int right = 0; right < n; right++) {
+            if (charIndex[s.charAt(right)] >= left) {
+                left = charIndex[s.charAt(right)] + 1;
+            }
+            charIndex[s.charAt(right)] = right;
+            longestSubLength = Math.max(longestSubLength, right - left + 1);
+        }
+
+        return longestSubLength;
+    }
+}
